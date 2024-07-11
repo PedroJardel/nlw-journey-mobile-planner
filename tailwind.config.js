a6d093ac6eb8c22f8baa420e114cbaef348b543f
Pedro Jardel Zago Lima <pedrozagolima@gmail.com>
2024-07-11 00:05:26 -0300

    feat: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// Importação do token para costumização do tema
+
+import { colors } from './src/styles/colors'
+import { fontFamily } from './src/styles/fontFamily'
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    presets: [require("nativewind/preset")],
+    theme: {
+      extend: {
+        colors,
+        fontFamily
+      },
+    },
+    plugins: [],
+  }
